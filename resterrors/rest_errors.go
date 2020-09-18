29d0f919d382573a4144a65f3e96d14bb2a927a4
dung997bn <phaichisophan@gmail.com>
2020-09-18 21:13:28 +0700

    initial version

---
+++ resterrors/rest_errors.go
@@ -0,0 +1,47 @@
+package resterrors
+
+import (
+	"errors"
+	"net/http"
+)
+
+//RestErr type
+type RestErr struct {
+	Message string        `json:"message"`
+	Status  int           `json:"status"`
+	Error   string        `json:"error"`
+	Causes  []interface{} `json:"causes"`
+}
+
+//NewBadRequestError create new bad request status with message
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+//NewNotFoundError create new not found status with message
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "bad_request",
+	}
+}
+
+//NewInternalServerError create new internal server error status with message
+func NewInternalServerError(message string, err error) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+		Causes:  []interface{}{err},
+	}
+}
+
+//NewError create new error
+func NewError(msg string) error {
+	return errors.New(msg)
+}
